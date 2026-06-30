3b4c0480cf28e0952edc5909d49e78239cdc32d6
Devarsh Vora <60837459+devarshvora@users.noreply.github.com>
2026-06-30 16:52:40 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Spark Field Estimator service worker
-const CACHE_NAME = 'spark-field-estimator-v61';
+const CACHE_NAME = 'spark-field-estimator-v64';
 const SCOPE = self.registration.scope;
 const toURL = (path) => new URL(path, SCOPE).href;
 const APP_SHELL = [
